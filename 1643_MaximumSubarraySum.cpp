379b306a3c917bb38986c6b7ac1b1b0866ec7ec0
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-06-11 13:36:05 +0700

    1643 - Maximum Subarray Sum

---
+++ 1643_MaximumSubarraySum.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using sint = signed int;
+#define ll long long
+#define ull unsigned ll
+#define int ll
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define vec vector
+
+#define LLMAX INT64_MAX
+
+#define in cin
+#define out cout
+#define input in
+#define print out
+#define GET(var) getline(cin, var)
+
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define FORIR(i, a, n) for (int i = a; i < n; i++)
+#define FORI(i, n) FORIR(i, 0, n)
+#define FORR(a, n) FORIR(i, a, n)
+#define FOR(n) FORIR(i, 0, n)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+template <class T>
+void OFILE(T finp, T fout)
+{
+    freopen(finp, "r", stdin), freopen(fout, "w", stdout);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> pref(n + 1);
+    for (int i = 1; i <= n; ++i)
+        cin >> pref[i], pref[i] += pref[i - 1];
+
+    vector<int> mi(n + 1);
+    for (int i = 1; i <= n; ++i)
+        mi[i] = min(mi[i - 1], pref[i]);
+
+    int result = -LLMAX;
+    for (int i = 1; i <= n; ++i)
+        result = max(result, pref[i] - mi[i - 1]);
+
+    cout << result;
+}
+
+sint main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+
+    solve();
+
+    return 0;
+}
