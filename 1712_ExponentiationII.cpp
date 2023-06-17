348d01270037ffce4cc65483bb864a7111812ae3
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-06-17 12:02:20 +0700

    1712 - Exponentation II

---
+++ 1712_ExponentiationII.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+const int MOD = 1000000007;
+
+int binpow(int a, int b, int mod)
+{
+    if (!b)
+        return 1;
+    
+    int t = binpow(a, b / 2, mod);
+    return t * t % mod * IF(b & 1, a, 1) % mod;
+}
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << binpow(a, binpow(b, c, MOD - 1), MOD) << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
