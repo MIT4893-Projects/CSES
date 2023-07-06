738ba2537ac279a59571d978df58e7b107b3006f
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-07-06 16:16:00 +0700

    1618 - Trailing Zeros

---
+++ 1618_TrailingZeros.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int pow5 = 5;
+    int result = 0;
+    while (pow5 <= n)
+    {
+        result += n / pow5;
+        pow5 *= 5;
+    }
+
+    cout << result;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    solve();
+
+    return 0;
+}
