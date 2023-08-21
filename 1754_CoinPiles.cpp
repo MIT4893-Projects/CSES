526b2822086cff855e4fd031396066d45fd2eab5
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-08-21 19:44:59 +0700

    1754 - Coin Piles

---
+++ 1754_CoinPiles.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define endl "\n"
+#define LLMAX INT64_MAX
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << IF((a + b) % 3 == 0 && min(a, b) >= max(a, b) / 2, "YES", "NO") << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    MULTI
+    solve();
+
+    return 0;
+}
